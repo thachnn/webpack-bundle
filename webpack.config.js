eab30a8fe002cb8dd0338329126543c04421e716
Thach Nguyen <thachnn80@gmail.com>
2022-08-27 12:18:47 +0700

    Try to bundle packages `glob`, `ajv`, `terser`,...

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { TerserPlugin, CopyPlugin, BannerPlugin, ReplaceCodePlugin } = require('w
 
 /** @typedef {import("webpack").Configuration} Configuration */
 
-/** @typedef {{ from: String, to?: String | Function, context?: String, transform?: Function }} ObjectPattern */
+/** @typedef {{ from: String, to?: String | Function, context?: String, transform?: Function, transformAll?: Function }} ObjectPattern */
 
 /**
  * @param {String} name
@@ -48,6 +48,30 @@ const webpackConfig = (name, config, clean = name.charAt(0) !== '/') => ({
 
 /** @param {Array<ObjectPattern | string>} patterns */
 const newCopyPlugin = (patterns) => new CopyPlugin({ patterns });
+
+/** @param {Array<{ data: Buffer, absoluteFilename: String }>} assets */
+const combineDTS = (assets) => {
+  const baseDir = path.join(__dirname, 'node_modules');
+  const getModuleName = (file) =>
+    path
+      .relative(baseDir, file)
+      .replace(/\\+/g, '/')
+      .replace(/(\/index)?\.d\.ts$/i, '');
+
+  return assets.reduce((result, asset) => {
+    const moduleName = getModuleName(asset.absoluteFilename);
+    const assetDir = path.dirname(asset.absoluteFilename);
+
+    const content = String(asset.data)
+      .replace(/(^|\s)declare\s/gm, '$1')
+      .replace(
+        /^(\s*(?:im|ex)port .* from ['"])([./][^'"]*)/gm,
+        (_, m1, m2) => `${m1}${getModuleName(path.resolve(assetDir, m2))}`
+      );
+
+    return `${result}\ndeclare module '${moduleName}' {\n${content.trim()}\n}\n`;
+  }, '');
+};
 
 module.exports = [
   webpackConfig('glob', {
@@ -98,7 +122,11 @@ module.exports = [
             return JSON.stringify(pkg, null, 2);
           },
         },
-        // TODO dts-bundle --name fast-glob --main out/index.d.ts --baseDir . --out dist/index.d.ts
+        {
+          from: 'node_modules/fast-glob/out/{index,settings,types/index,managers/tasks}.d.ts',
+          to: 'out/index.d.ts',
+          transformAll: combineDTS,
+        },
       ]),
     ],
   }),
@@ -120,7 +148,64 @@ module.exports = [
       ]),
     ],
   }),
-  // TODO @wasm
+  webpackConfig('@wasm-ast', {
+    entry: { 'lib/index': './node_modules/@webassemblyjs/ast/esm/index' },
+    output: { libraryTarget: 'commonjs' },
+    target: 'node0.10',
+    plugins: [
+      newCopyPlugin([
+        { from: 'node_modules/@webassemblyjs/ast/{LICENSE*,README*}', to: '[name][ext]' },
+        {
+          from: 'node_modules/@webassemblyjs/ast/package.json',
+          transform(content) {
+            const { dependencies: _1, devDependencies: _2, module: _3, ...pkg } = JSON.parse(content);
+            return JSON.stringify(pkg, null, 2);
+          },
+        },
+      ]),
+    ],
+  }),
+  webpackConfig('@wasm-parser', {
+    entry: { 'lib/index': './node_modules/@webassemblyjs/wasm-parser/esm/index' },
+    output: { libraryTarget: 'commonjs' },
+    target: 'node0.10',
+    externals: ['@webassemblyjs/ast'],
+    plugins: [
+      newCopyPlugin([
+        { from: 'node_modules/@webassemblyjs/wasm-parser/{LICENSE*,README*}', to: '[name][ext]' },
+        {
+          from: 'node_modules/@webassemblyjs/wasm-parser/package.json',
+          transform(content) {
+            const { devDependencies: _1, module: _2, ...pkg } = JSON.parse(content);
+            pkg.dependencies = { '@webassemblyjs/ast': pkg.dependencies['@webassemblyjs/ast'] };
+            return JSON.stringify(pkg, null, 2);
+          },
+        },
+      ]),
+    ],
+  }),
+  webpackConfig('@wasm-edit', {
+    entry: { 'lib/index': './node_modules/@webassemblyjs/wasm-edit/esm/index' },
+    output: { libraryTarget: 'commonjs' },
+    target: 'node0.10',
+    externals: ['@webassemblyjs/ast', '@webassemblyjs/wasm-parser'],
+    plugins: [
+      newCopyPlugin([
+        { from: 'node_modules/@webassemblyjs/wasm-edit/{LICENSE*,README*}', to: '[name][ext]' },
+        {
+          from: 'node_modules/@webassemblyjs/wasm-edit/package.json',
+          transform(content) {
+            const { devDependencies: _1, module: _2, ...pkg } = JSON.parse(content);
+            pkg.dependencies = {
+              '@webassemblyjs/ast': pkg.dependencies['@webassemblyjs/ast'],
+              '@webassemblyjs/wasm-parser': pkg.dependencies['@webassemblyjs/wasm-parser'],
+            };
+            return JSON.stringify(pkg, null, 2);
+          },
+        },
+      ]),
+    ],
+  }),
   webpackConfig('terser', {
     entry: {
       main: { import: './node_modules/terser/main', library: { name: 'Terser', type: 'umd' } },
